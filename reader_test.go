e84d6dff1f3f73d46088737bc7c9359efc2d2329
Olaf Alders <olaf@wundersolutions.com>
2023-06-06 18:55:11 -0400

    Add maybeReadINI tests for sad path

--- reader_test.go
+++ reader_test.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMaybeReadINI(t *testing.T) {
-	release, err := maybeReadINI("testdata/etc/os-release")
-	assert.NoError(t, err)
-	assert.Equal(t, "18.04", release.VersionID)
-	fmt.Printf("%+v", release)
+	{
+		release, err := maybeReadINI("testdata/etc/os-release")
+		assert.NoError(t, err)
+		assert.Equal(t, "18.04", release.VersionID)
+	}
+	{
+		// if the file does not exist on this system, that's not an error
+		release, err := maybeReadINI("testdata/etc/os-releasezzz")
+		assert.NoError(t, err)
+		assert.Nil(t, release)
+	}
+	{
+		// if the file cannot be parsed, that's an error
+		release, err := maybeReadINI("testdata/etc/not-an-ini-file")
+		assert.Error(t, err)
+		assert.Nil(t, release)
+	}
 }
