4e8f97cf6ae14297991195b362d546d5157538d8
Olaf Alders <olaf@wundersolutions.com>
2023-06-13 21:30:10 +0000

    Add more help text

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 
 var api struct {
 	Debug   bool             `help:"turn on debugging statements"`
-	OS      OSCmd            `cmd:""`
-	CLI     CLICmd           `cmd:""`
+	OS      OSCmd            `cmd:"" help:"Check OS attributes. e.g. \"is os name eq darwin\""`
+	CLI     CLICmd           `cmd:"" help:"Check cli version. e.g. \"is cli version tmux gte 3\""`
 	Known   KnownCmd         `cmd:""`
-	There   ThereCmd         `cmd:"" help:"Check if command exists"`
+	There   ThereCmd         `cmd:"" help:"Check if command exists. e.g. \"is there git\""`
 	Version kong.VersionFlag `help:"Print version to screen"`
 }
 
@@ -26,9 +26,9 @@ type Context struct {
 type CLICmd struct {
 	Version struct {
 		Name string `arg:"" required:"" help:"[name of command or path to command]"`
-		Op   string `arg:"" required:"" enum:"eq,ne,gt,gte,lt,lte"`
+		Op   string `arg:"" required:"" enum:"eq,ne,gt,gte,lt,lte" help:"[eq|ne|gt|gte|lt|lte]"`
 		Val  string `arg:"" required:""`
-	} `cmd:"" help:"Check OS version"`
+	} `cmd:"" help:"Check version of command. e.g. \"is cli version tmux gte 3\""`
 }
 
 // OSCmd type is configuration for OS level checks
@@ -42,11 +42,11 @@ type OSCmd struct {
 type KnownCmd struct {
 	OS struct {
 		Attr string `arg:"" required:"" name:"attribute" help:"[arch|id|id-like|pretty-name|name|version|version-codename]"`
-	} `cmd:"" help:"Print without testing condition. e.g. \"is known os name\""`
+	} `cmd:"" help:"Print without check. e.g. \"is known os name\""`
 	CLI struct {
 		Attr string `arg:"" name:"attribute" required:"" enum:"version"`
 		Name string `arg:"" required:""`
-	} `cmd:"" help:"Print without testing condition. e.g. \"is known cli version git\""`
+	} `cmd:"" help:"Print without check. e.g. \"is known cli version git\""`
 }
 
 // ThereCmd is configuration for finding executables
