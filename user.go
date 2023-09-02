01743b3abc6addffa7d55cbd5731fcc1f85de105
Olaf Alders <olaf@wundersolutions.com>
2023-09-02 12:01:14 -0400

    Wrap more errors and reduce complexity

--- user.go
+++ user.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"errors"
 	"io"
 	"log"
 	"os/exec"
@@ -12,32 +13,34 @@ import (
 
 // Run "is user ...".
 func (r *UserCmd) Run(ctx *types.Context) error {
-	if r.Sudoer != "" {
-		if ctx.Debug {
-			log.Printf("Running \"sudo -n true\"\n")
-		}
-		cmd := exec.Command("sudo", "-n", "true")
-		stderr, err := cmd.StderrPipe()
-		if err != nil {
-			return err
-		}
-
-		if err := cmd.Start(); err != nil {
-			return err
-		}
-
-		slurp, _ := io.ReadAll(stderr)
-		if ctx.Debug {
-			log.Printf("STDERR: %s", string(slurp))
-		}
-		if strings.Contains(string(slurp), "password") {
-			return nil
-		}
-
-		if err := cmd.Wait(); err != nil {
-			return err
-		}
-		ctx.Success = true
+	if r.Sudoer == "" {
+		return errors.New("user command does not exist")
 	}
+
+	if ctx.Debug {
+		log.Printf("Running \"sudo -n true\"\n")
+	}
+	cmd := exec.Command("sudo", "-n", "true")
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return errors.Join(errors.New("open STDERR pipe"), err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		return errors.Join(errors.New("starting command"), err)
+	}
+
+	slurp, _ := io.ReadAll(stderr)
+	if ctx.Debug {
+		log.Printf("STDERR: %s", string(slurp))
+	}
+	if strings.Contains(string(slurp), "password") {
+		return nil
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return errors.Join(errors.New("waiting for command"), err)
+	}
+	ctx.Success = true
 	return nil
 }
