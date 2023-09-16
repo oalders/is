3b62f498f3888fe8dfb18330ef992234c84fdb1d
Olaf Alders <olaf@wundersolutions.com>
2023-09-15 21:17:21 -0400

    Bump version to 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changes
 
-## 0.1.3 - 2023-09-15
+## 0.2.0 - 2023-09-15
 
 - Add "is command output"
 
--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 
 	ctx := kong.Parse(&API,
 		kong.Vars{
-			"version": "0.1.3",
+			"version": "0.2.0",
 		})
 	runContext := types.Context{Debug: API.Debug}
 	err := ctx.Run(&runContext)
