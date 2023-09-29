f15696dc4057e09f2208f45ca6ae09d36e91e5aa
Olaf Alders <olaf@wundersolutions.com>
2023-09-28 20:41:55 -0400

    Convert tests to be table-driven

--- mac/mac_test.go
+++ mac/mac_test.go
@@ -10,20 +10,26 @@ import (
 
 func TestCodeName(t *testing.T) {
 	t.Parallel()
-	assert.Equal(t, "ventura", mac.CodeName("13.1"))
-	assert.Equal(t, "monterey", mac.CodeName("12.2"))
-	assert.Equal(t, "big sur", mac.CodeName("11.2"))
-	assert.Equal(t, "catalina", mac.CodeName("10.15"))
-	assert.Equal(t, "mojave", mac.CodeName("10.14"))
-	assert.Equal(t, "high sierra", mac.CodeName("10.13"))
-	assert.Equal(t, "sierra", mac.CodeName("10.12"))
-	assert.Equal(t, "el capitan", mac.CodeName("10.11"))
-	assert.Equal(t, "yosemite", mac.CodeName("10.10"))
-	assert.Equal(t, "mavericks", mac.CodeName("10.9"))
-	assert.Equal(t, "mountain lion", mac.CodeName("10.8"))
-	assert.Equal(t, "", mac.CodeName("10.7"))
-	assert.Equal(t, "", mac.CodeName("9.0"))
-	assert.Equal(t, "", mac.CodeName("-1"))
+	tests := [][]string{
+		{"13.1", "ventura"},
+		{"12.2", "monterey"},
+		{"11.2", "big sur"},
+		{"10.15", "catalina"},
+		{"10.14", "mojave"},
+		{"10.13", "high sierra"},
+		{"10.12", "sierra"},
+		{"10.11", "el capitan"},
+		{"10.10", "yosemite"},
+		{"10.9", "mavericks"},
+		{"10.8", "mountain lion"},
+		{"10.7", ""},
+		{"9.0", ""},
+		{"-1", ""},
+	}
+
+	for _, v := range tests {
+		assert.Equal(t, v[1], mac.CodeName(v[0]))
+	}
 }
 
 func TestVersion(t *testing.T) {
