793cb1f8bb9719c506534f5e6817ef3a1560ca26
Olaf Alders <olaf@wundersolutions.com>
2025-06-27 07:14:48 -0400

    Add some tests for markdown

--- known_test.go
+++ known_test.go
@@ -281,3 +281,67 @@ func Test_envSummary(t *testing.T) {
 		assert.Contains(t, output.String(), "/usr/bin")
 	})
 }
+
+func TestMarkdown(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct { //nolint:govet
+		name     string
+		headers  []string
+		rows     [][]string
+		expected string
+	}{
+		{
+			name:     "empty input",
+			headers:  []string{},
+			rows:     [][]string{},
+			expected: "|  |\n|\n",
+		},
+		{
+			name:    "single header, no rows",
+			headers: []string{"Header"},
+			rows:    [][]string{},
+			expected: "| Header |\n" +
+				"|---|\n",
+		},
+		{
+			name:    "multiple headers, no rows",
+			headers: []string{"Header1", "Header2", "Header3"},
+			rows:    [][]string{},
+			expected: "| Header1 | Header2 | Header3 |\n" +
+				"|---|---|---|\n",
+		},
+		{
+			name:    "with data rows",
+			headers: []string{"Name", "Value"},
+			rows: [][]string{
+				{"key1", "value1"},
+				{"key2", "value2"},
+			},
+			expected: "| Name | Value |\n" +
+				"|---|---|\n" +
+				"| key1 | value1 |\n" +
+				"| key2 | value2 |\n",
+		},
+		{
+			name:    "with newlines in data",
+			headers: []string{"Name", "Value"},
+			rows: [][]string{
+				{"key1", "line1\nline2"},
+				{"key2", "value2"},
+			},
+			expected: "| Name | Value |\n" +
+				"|---|---|\n" +
+				"| key1 | line1<br>line2 |\n" +
+				"| key2 | value2 |\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			result := markdown(tt.headers, tt.rows)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
